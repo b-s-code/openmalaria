f88b53b7fa02eca09c52a78572cddcff66a6de1d
b-s-code <89502375+b-s-code@users.noreply.github.com>
2025-09-05 13:50:19 +0800

    Add modified cohort plotting code.

--- util/example/plot.py
+++ util/example/plot.py
@@ -19,120 +19,52 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+output_txt_path = "/home/bs/sync/code/fork/openMalaria-48.0/output.txt"
+
 from matplotlib import pyplot as plt
 import pandas as pd
 
 mm = {
 	0: 'nHost',
 	1: 'nInfect',
-	2: 'nExpectd',
-	3: 'nPatent',
-	4: 'sumLogPyrogenThres',
-	5: 'sumlogDens',
-	6: 'totalInfs',
-	7: 'nTransmit',
-	8: 'totalPatentInf',
-	9: 'contrib',
-	10: 'sumPyrogenThresh',
-	11: 'nTreatments1',
-	12: 'nTreatments2',
-	13: 'nTreatments3',
-	14: 'nUncomp',
-	15: 'nSevere',
-	16: 'nSeq',
-	17: 'nHospitalDeaths',
-	18: 'nIndDeaths',
-	19: 'nDirDeaths',
-	20: 'nEPIVaccinations',
-	21: 'allCauseIMR',
-	22: 'nMassVaccinations',
-	23: 'nHospitalRecovs',
-	24: 'nHospitalSeqs',
-	25: 'nIPTDoses',
-	26: 'annAvgK',
-	27: 'nNMFever',
-	30: 'innoculationsPerAgeGroup',
-	28: 'innoculationsPerDayOfYear',
-	29: 'kappaPerDayOfYear',
-	31: 'Vector_Nv0',
-	32: 'Vector_Nv',
-	33: 'Vector_Ov',
-	34: 'Vector_Sv',
-    35: 'InputEIR',
-    36: 'SimulatedEIR',
-	39: 'Clinical_RDTs',
-	40: 'Clinical_DrugUsage',
-	41: 'Clinical_FirstDayDeaths',
-	42: 'Clinical_HospitalFirstDayDeaths',
-	43: 'nNewInfections',
-	44: 'nMassITNs',
-	45: 'nEPI_ITNs',
-	46: 'nMassIRS',
-	47: 'nMassVA',
-	48: 'Clinical_Microscopy',
-	49: 'Clinical_DrugUsageIV',
-	50: 'nAddedToCohort',
-	51: 'nRemovedFromCohort',
-	52: 'nMDAs',
-	53: 'nNmfDeaths',
-	54: 'nAntibioticTreatments',
-	55: 'nMassScreenings',
-	56: 'nMassGVI',
-	57: 'nCtsIRS',
-	58: 'nCtsGVI',
-	59: 'nCtsMDA',
-	60: 'nCtsScreenings',
-	61: 'nSubPopRemovalTooOld',
-	62: 'nSubPopRemovalFirstEvent',
-	63: 'nPQTreatments',
-	64: 'nTreatDiagnostics',
-	65: 'nMassRecruitOnly',
-	66: 'nCtsRecruitOnly',
-	67: 'nTreatDeployments',
-	68: 'sumAge',
-	69: 'nInfectByGenotype',
-	70: 'nPatentByGenotype',
-	71: 'logDensByGenotype',
-	72: 'nHostDrugConcNonZero',
-	73: 'sumLogDrugConcNonZero',
-	74:	'expectedDirectDeaths',
-	75:	'expectedHospitalDeaths',
-	76:	'expectedIndirectDeaths',
-	77:	'expectedSequelae',
-	78:	'expectedSevere',
-	79:	'innoculationsPerVector',
-	80:	'nSevereWithoutComorbidities',
-	81:	'expectedSevereWithoutComorbidities'	
 }
 
 mmi = {v: k for k, v in mm.items()}
 
-df = pd.read_csv(f'output.txt', sep="\t", header=1)
+df = pd.read_csv(output_txt_path, sep="\t", header=1)
 df.columns = ['survey', 'age-group', 'measure', 'value']
 
-nHosts = df[(df["measure"] == mmi['nHost'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()#.values
-inputEIR = df[(df["measure"] == mmi['InputEIR'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()
-simulatedEIR = df[(df["measure"] == mmi['SimulatedEIR'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()
-innoculations = df[(df["measure"] == mmi['innoculationsPerAgeGroup'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()#.values
-nPatent = df[(df["measure"] == mmi['nPatent'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()#.values
-nUncomp = df[(df["measure"] == mmi['nUncomp'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()#.values
+nHosts_ageGp1 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 1)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp2 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 2)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp3 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 3)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp4 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 4)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp5 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 5)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp6 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 6)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp1001 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 1001)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp1002 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 1002)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp1003 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 1003)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp1004 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 1004)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp1005 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 1005)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts_ageGp1006 = df[(df["measure"] == mmi['nHost']) & (df['age-group'] == 1006)].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nHosts = df[(df["measure"] == mmi['nHost'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()
+nInfect = df[(df["measure"] == mmi['nInfect'])].groupby(['survey', 'measure']).sum().value[1:].reset_index()
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-ax.plot(inputEIR.index, inputEIR.value, marker="v", markersize=1, label=f"inputEIR")
-ax.plot(simulatedEIR.index, simulatedEIR.value, marker="v", markersize=1, label=f"simulatedEIR")
-ax.legend(loc="upper left")
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-prevalence = nPatent / nHosts
-ax.plot(prevalence.index, prevalence.value, marker="v", markersize=1, label=f"Prevalence")
-ax.legend(loc="upper left")
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ci = nUncomp / nHosts
-ax.plot(ci.index, ci.value, marker="v", markersize=1, label=f"Clinical Incidence")
+ax.plot(nHosts.index, nHosts.value, marker="v", markersize=1, label=f"nHosts")
+ax.plot(nInfect.index, nInfect.value, marker="v", markersize=1, label=f"nInfect")
+ax.plot(nHosts_ageGp1.index, nHosts_ageGp1.value, marker="v", markersize=1, label=f"nHosts_ageGp1")
+ax.plot(nHosts_ageGp2.index, nHosts_ageGp2.value, marker="v", markersize=1, label=f"nHosts_ageGp2")
+ax.plot(nHosts_ageGp3.index, nHosts_ageGp3.value, marker="v", markersize=1, label=f"nHosts_ageGp3")
+ax.plot(nHosts_ageGp4.index, nHosts_ageGp4.value, marker="v", markersize=1, label=f"nHosts_ageGp4")
+ax.plot(nHosts_ageGp5.index, nHosts_ageGp5.value, marker="v", markersize=1, label=f"nHosts_ageGp5")
+ax.plot(nHosts_ageGp6.index, nHosts_ageGp6.value, marker="v", markersize=1, label=f"nHosts_ageGp6")
+ax.plot(nHosts_ageGp1001.index, nHosts_ageGp1001.value, marker="v", markersize=1, label=f"nHosts_ageGp1001")
+ax.plot(nHosts_ageGp1002.index, nHosts_ageGp1002.value, marker="v", markersize=1, label=f"nHosts_ageGp1002")
+ax.plot(nHosts_ageGp1003.index, nHosts_ageGp1003.value, marker="v", markersize=1, label=f"nHosts_ageGp1003")
+ax.plot(nHosts_ageGp1004.index, nHosts_ageGp1004.value, marker="v", markersize=1, label=f"nHosts_ageGp1004")
+ax.plot(nHosts_ageGp1005.index, nHosts_ageGp1005.value, marker="v", markersize=1, label=f"nHosts_ageGp1005")
+ax.plot(nHosts_ageGp1006.index, nHosts_ageGp1006.value, marker="v", markersize=1, label=f"nHosts_ageGp1006")
 ax.legend(loc="upper left")
 
 plt.show()
